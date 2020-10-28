7c403e109a229c02ab33ba17df55fd888c94fe26
潘少军 <1760406005@qq.com>
2020-10-28 17:54:55 +0800

    222

--- api/controllers/StudyOptController.js
+++ api/controllers/StudyOptController.js
@@ -4,7 +4,7 @@ var request = require("request");
 var dirPath = path.join(__dirname, "../../assets/file");
 
 module.exports = {
-    get: async (req, res) => {
+    find: async (req, res) => {
         try {
             let data = await Study.find({});
             res.json({
@@ -19,7 +19,7 @@ module.exports = {
         }
     },
 
-    upd: async (req, res) => {
+    create: async (req, res) => {
         let { date, indexs } = req.body;
         if (date && indexs) {
             try {
@@ -42,84 +42,6 @@ module.exports = {
         }
     },
 
-    store: async (req, res) => {
-        let { word,explain, url, fileName } = req.body;
-        if (explain && url && fileName) {
-            const fileTempUrl = path.join(dirPath, fileName)
-            if (fs.existsSync(fileTempUrl)) {//文件已经存在
-                try {
-                    let data = await Bing.create({ word,explain, file: "file/" + fileName }).fetch();
-                    if (data) {
-                        res.json({
-                            code: 0,
-                            msg: "文件已存在,传输到数据库成功"
-                        })
-                    } else {
-                        res.json({
-                            code: 1,
-                            msg: "文件已存在，数据库连接错误？"
-                        })
-                    }
-                } catch (err) {
-                    res.json({
-                        code: 2,
-                        msg: "文件已存在，数据库已存在"
-                    })
-                }
-            } else {//文件未存在
-                let stream = fs.createWriteStream(fileTempUrl);
-                request(url).pipe(stream).on("close", async (err) => {
-                    if (err) {
-                        res.json({
-                            code: 1,
-                            msg: "文件下载失败"
-                        })
-                    } else {
-                        try {
-                            let data = await Bing.create({ word,explain, file: "file/" + fileName }).fetch();
-                            if (data) {
-                                res.json({
-                                    code: 0,
-                                    msg: "文件下载完毕,传输到数据库成功"
-                                })
-                            } else {
-                                res.json({
-                                    code: 1,
-                                    msg: "文件下载完毕,数据连接错误？"
-                                })
-                            }
-                        } catch (err) {
-                            res.json({
-                                code: 2,
-                                msg: "文件下载完毕,数据库连接有问题"
-                            })
-                        }
-                    }
-                });
-            }
-        }else{
-            res.json({
-                code:404,
-                msg:"你是来捣乱的吧？"
-            })
-        }
-    },
-
-    getStoreInfo: async (req,res)=>{
-        try{
-            let data=await Bing.find({
-                select:['word']
-            });
-            res.json({
-                code:0,
-                data
-            });
-        }catch(err){
-            res.json({
-                code:1,
-                msg:err
-            })
-        }
-    }
+    
 
 }
--- config/policies.js
+++ config/policies.js
@@ -16,7 +16,7 @@ module.exports.policies = {
   * (`true` allows public access)                                            *
   *                                                                          *
   ***************************************************************************/
-  '*': false,
+  '*': true,
   StudyOptController: {
     '*':true
   }
