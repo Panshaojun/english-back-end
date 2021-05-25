1b6ccba737389322bcb1daea03d6ca7aa9af19ac
潘少军 <1760406005@qq.com>
2021-05-25 18:27:31 +0800

    upd all

---
+++ api/models/Kaoyanbing.js
@@ -0,0 +1,12 @@
+module.exports = {
+    attributes: {
+        key: { type: 'string', required: true },
+        definition:{type:'string',required: true,},
+        auido:{type:'string',required: true,},
+        word_change:{type:'string',required: true,},
+        examples:{type:'string',required: true,},
+        english_chinese:{type:'string',required: true,},
+        idioms:{type:'string',required: true,},
+        sider:{type:'string',required: true,},
+    },
+};
