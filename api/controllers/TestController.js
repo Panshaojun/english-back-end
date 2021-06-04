e1588836ef6798ecf02184ec5622a43699aacebc
潘少军 <1760406005@qq.com>
2021-06-04 18:11:30 +0800

    del 没有的

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -4,42 +4,27 @@ const request = require("request");
 const dirPath = path.join(__dirname, "../../assets/file");
 
 module.exports = {
-    create: async (req, res) => {
-        let { id, w, short, long } = req.body;
-        if (id && w && short && long) {
-            await Vocabularykaoyan.create({ id, w, short, long }).fetch();
-            res.json({
-                code: 0,
-                msg: "成功了"
-            })
-        } else {
-            res.json({
-                code: 101,
-                msg: "没有"
-            })
-        }
-    },
     fetchfile: async (req, res) => {
         let { fileUrl,fileName } = req.body;
         if (fileUrl && fileName) {
             const fileTempUrl = path.join(dirPath, fileName)
             if (fs.existsSync(fileTempUrl)) {//文件已经存在
                 res.json({
-                    code: 2,
-                    msg: "文件已存在"
+                    code: 0,
+                    data: "/model/file/"+fileName
                 })
             } else {//文件未存在
                 const stream = fs.createWriteStream(fileTempUrl);
                 request(fileUrl).pipe(stream).on("close",(err) => {
                     if (err) {
                         res.json({
-                            code: 1,
-                            msg: "文件下载失败"
+                            code: 0,
+                            data: fileUrl
                         })
                     } else {
                         res.json({
                             code: 0,
-                            msg: "文件下载完毕"
+                            msg: "/model/file/"+fileName
                         })
                     }
                 });
